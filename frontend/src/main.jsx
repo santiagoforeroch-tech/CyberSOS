38931bc3d7aff62c139d2746ae9be1abb45f588c
Santiago <santiagoforeroch@gmail.com>
2026-09-13 21:00:18 -0500

    Make canonical CyberSOS theme override legacy styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 import './styles.css'
+import './canonical-theme.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
